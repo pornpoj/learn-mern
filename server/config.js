8f2d0b15723c231daa0dadc46eb22b573c2a9f52
pornpoj <khowoat9348@gmail.com>
2016-03-27 13:27:47 +0700

    editnaja

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://52.207.215.58:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
